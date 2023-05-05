cbd0d662979b8b52813ff775b3ab98020cae2b52
guangxue <contact.guangxue@gmail.com>
2023-05-05 14:30:48 +1000

    [chore] use `path.resolve` instead of hardcoded

--- app/(auth)/api/blog/uploads/route.ts
+++ app/(auth)/api/blog/uploads/route.ts
@@ -4,13 +4,16 @@ import path from "path";
 
 export async function POST(req: NextRequest) {
   const fileInfo = await req.json();
-  // console.log(fileInfo);
-  const rootPath = path.resolve("/");
-  const savePath = `/Users/Guangxue/Codes/digitalocean/www/guangxuezhang.com/public/${fileInfo.name}`;
+
+  const rootPath = path.resolve("./");
+  const destPath = `/public/${fileInfo.name}`;
+  const savePath = path.join(rootPath, destPath);
+
   const imageStr = fileInfo.file.substring("data:image/jpeg;base64".length);
   const base64Buf = Buffer.from(imageStr, "base64");
+
   try {
-    await writeFile(path.join(savePath), base64Buf, {
+    await writeFile(savePath, base64Buf, {
       flag: "wx+",
     });
     return new Response("uploaded", {
