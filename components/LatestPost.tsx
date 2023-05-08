efdb404de8e95fac41dec6e442fac34b75b9a1ea
guangxue <contact.guangxue@gmail.com>
2023-05-09 09:43:16 +1000

    [fixbug] fix latest articles logo size

--- components/LatestPost.tsx
+++ components/LatestPost.tsx
@@ -14,9 +14,9 @@ export default function LatestPost({ post }: LastestPostProps) {
   return (
     <div className="LatestPost flex sm:basis-full md:basis-[46%] lg:basis-[32%] bg-neutral-50">
       <div className="pic m-3">
-        <Image src={post.logo} alt="" width={60} height={60} />
+        <Image src={post.logo} alt="" width={39} height={39} />
       </div>
-      <article className="desc m-1">
+      <article className="desc m-1 basis-[60%]">
         <Link href={`blog/${post.slug}`}>
           <h3>{post.title}</h3>
         </Link>
