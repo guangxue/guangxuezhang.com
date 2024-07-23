d426aa61e25d2d4abd7d42a53a493075b6504d62
guangxue <contact@guangxuezhang.com>
2024-07-23 16:36:07 +1000

    initial commit

---
+++ app/writing/writing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'writing',
+  standalone: true,
+  imports: [],
+  templateUrl: './writing.component.html',
+  styleUrl: './writing.component.css'
+})
+export class WritingComponent {
+
+}
